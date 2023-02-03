9bb6c12fcb3592072abfe76e8442034c4d866b7f
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-04 00:25:46 +0100

    Add filter by categories feature

--- src/js/components/Songs.js
+++ src/js/components/Songs.js
@@ -5,6 +5,8 @@ export class Songs {
     const thisSong = this;
 
     thisSong.render(data);
+    thisSong.generateCategoryList(data);
+    thisSong.filterSongs();
   }
 
   render(data) {
@@ -12,10 +14,10 @@ export class Songs {
 
     const generatedHTML = templates.songWrapper(data);
 
-    const homePageContainer = document.querySelector(
+    thisSong.homePageContainer = document.querySelector(
       select.containerOf.homePage
     );
-    homePageContainer.innerHTML = generatedHTML;
+    thisSong.homePageContainer.innerHTML = generatedHTML;
     thisSong.initMusicPlayerWidget();
   }
 
@@ -26,5 +28,79 @@ export class Songs {
       selector: '.home-wrapper  .song-player',
       stopOthersOnPlay: true,
     });
+  }
+
+  generateCategoryList(data) {
+    const thisSong = this;
+    thisSong.songs = data;
+
+    const songsCategories = [];
+    thisSong.categoriesList = document.getElementById('categories-list');
+
+    for (let song of thisSong.songs) {
+      for (let category of song.categories) {
+        if (!songsCategories.includes(category)) {
+          songsCategories.push(category);
+        }
+      }
+    }
+
+    songsCategories.forEach((category) => {
+      let li = document.createElement('li');
+      li.innerText = category;
+      thisSong.categoriesList.appendChild(li);
+    });
+  }
+
+  filterSongs() {
+    const thisSong = this;
+    const filteredCategories = [];
+
+    for (let category of thisSong.categoriesList.children) {
+      category.addEventListener('click', function () {
+        const clickedCategory = this;
+        clickedCategory.classList.toggle('selected');
+
+        if (clickedCategory.classList.contains('selected')) {
+          if (!filteredCategories.includes(clickedCategory.innerHTML)) {
+            filteredCategories.push(clickedCategory.innerHTML);
+          }
+        } else {
+          const removeCategory = filteredCategories.indexOf(
+            clickedCategory.innerHTML
+          );
+
+          filteredCategories.splice(removeCategory, 1);
+        }
+
+        for (let songWrapper of thisSong.homePageContainer.children) {
+          let shouldBeHidden = false;
+
+          const songWrapperDescription =
+            songWrapper.querySelector('.song-description');
+
+          const songWrapperIncludedText =
+            songWrapperDescription.firstElementChild.firstElementChild.innerHTML
+              .replace('Categories:', '')
+              .trim()
+              .slice(0, -1);
+
+          const songWrapperCategories = songWrapperIncludedText.split(',  ');
+
+          for (let filter of filteredCategories) {
+            if (!songWrapperCategories.includes(filter)) {
+              shouldBeHidden = true;
+              break;
+            }
+          }
+
+          if (shouldBeHidden) {
+            songWrapper.classList.add('hidden');
+          } else {
+            songWrapper.classList.remove('hidden');
+          }
+        }
+      });
+    }
   }
 }
