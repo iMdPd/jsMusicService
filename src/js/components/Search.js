2c32f42bdb059e44de956012965603e43d689025
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-04 20:45:09 +0100

    Add commits in Search.js

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -8,7 +8,7 @@ export class Search {
     thisSearch.getData(data);
     thisSearch.filterElements();
     thisSearch.setDefaultValues();
-    thisSearch.generateCategoryInputList(data);
+    thisSearch.generateCategorySelect();
   }
 
   render(element) {
@@ -21,10 +21,33 @@ export class Search {
     thisSearch.dom.wrapper.innerHTML = generatedHTML;
   }
 
-  setDefaultValues() {
+  getData(data) {
     const thisSearch = this;
 
-    const navSearchLink = document.querySelector(select.nav.searchLink);
+    thisSearch.dataSongs = data;
+
+    thisSearch.navSearchLink = document.querySelector(select.nav.searchLink);
+
+    thisSearch.button = thisSearch.dom.wrapper.querySelector(
+      select.searchPage.button
+    );
+    thisSearch.input = thisSearch.dom.wrapper.querySelector(
+      select.searchPage.input
+    );
+    thisSearch.categorySelector = thisSearch.dom.wrapper.querySelector(
+      select.searchPage.select
+    );
+    thisSearch.alerts = document.querySelectorAll(select.searchPage.alerts);
+    thisSearch.songsContainer = document.querySelector(
+      select.searchPage.songsContainer
+    );
+    thisSearch.songsCounter = document.querySelector(
+      select.searchPage.songsCounter
+    );
+  }
+
+  setDefaultValues() {
+    const thisSearch = this;
 
     /* logic teaken from https://www.seanmcp.com/articles/event-listener-for-class-change/ */
     function callback(mutationsList) {
@@ -36,95 +59,94 @@ export class Search {
           thisSearch.songsCounter.classList.add(classList.hidden);
           thisSearch.songsContainer.innerHTML = '';
           thisSearch.input.value = null;
-          thisSearch.select.value = thisSearch.select.children[0].value;
+          thisSearch.categorySelector.value =
+            thisSearch.categorySelector.children[0].value;
         }
       });
     }
 
     const mutationObserver = new MutationObserver(callback);
 
-    mutationObserver.observe(navSearchLink, { attributes: true });
-  }
-
-  getData(data) {
-    const thisSearch = this;
-
-    thisSearch.dataSongs = data;
-
-    thisSearch.button = thisSearch.dom.wrapper.querySelector(
-      select.searchPage.button
-    );
-    thisSearch.input = thisSearch.dom.wrapper.querySelector(
-      select.searchPage.input
-    );
-    thisSearch.select = thisSearch.dom.wrapper.querySelector(
-      select.searchPage.select
-    );
-
-    thisSearch.alerts = document.querySelectorAll(select.searchPage.alerts);
-    thisSearch.songsContainer = document.querySelector(
-      select.searchPage.songsContainer
-    );
-    thisSearch.songsCounter = document.querySelector(
-      select.searchPage.songsCounter
-    );
+    mutationObserver.observe(thisSearch.navSearchLink, { attributes: true });
   }
 
   filterElements() {
     const thisSearch = this;
-
+    /* add event listener on button search */
     thisSearch.button.addEventListener('click', function () {
       if (
+        /* if name input is empty and category select has set default option */
         thisSearch.input.value == '' &&
-        thisSearch.select.value == thisSearch.select.children[0].value
+        thisSearch.categorySelector.value ==
+          thisSearch.categorySelector.children[0].value
       ) {
+        /* based on the song list generate HTML element using template songWrapper */
         const generatedHTML = templates.songWrapper(thisSearch.dataSongs);
-
+        /* set generatedHTML as innerHTML of songsContainer  */
         thisSearch.songsContainer.innerHTML = generatedHTML;
-
+        /* add song counter */
         thisSearch.songsCounter.innerHTML =
           'We have found ' + thisSearch.dataSongs.length + ' songs...';
       } else if (
+        /* if name input has value and category select has set default option  */
         thisSearch.input.value &&
-        thisSearch.select.value == thisSearch.select.children[0].value
+        thisSearch.categorySelector.value ==
+          thisSearch.categorySelector.children[0].value
       ) {
+        /* set array named filteredSongs which filter for each song from songs list */
         const filteredSongs = thisSearch.dataSongs.filter((song) => {
           return (
+            /* get song.author name and lowercase it, then merge author name */
             song.author
               .toLowerCase()
               .replaceAll(' ', '')
+              /* check if song.author name includes input value */
               .includes(thisSearch.input.value.toLowerCase()) ||
+            /* and get song.title and lowercase it, then merge song title */
             song.title
               .toLowerCase()
               .replaceAll(' ', '')
+              /* check if song.title includes input value */
               .includes(thisSearch.input.value.toLowerCase())
           );
         });
 
+        /* if filteredSongs array has no contents */
         if (filteredSongs.length == 0) {
+          /* remove all content from songsContainer */
           thisSearch.songsContainer.innerHTML = '';
-
+          /* add song counter */
           thisSearch.songsCounter.innerHTML =
             'We have found ' + filteredSongs.length + ' songs...';
         } else {
+          /* if filteredSongs array has one element */
           if (filteredSongs.length === 1) {
+            /* add song counter */
             thisSearch.songsCounter.innerHTML =
               'We have found ' + filteredSongs.length + ' song...';
+            /* if filteredSongs array has less than one or more than one elements */
           } else {
+            /* add song counter */
             thisSearch.songsCounter.innerHTML =
               'We have found ' + filteredSongs.length + ' songs...';
           }
-
+          /* based on the song list generate HTML element using template songWrapper */
           const generatedHTML = templates.songWrapper(filteredSongs);
+          /* set generatedHTML as innerHTML of songsContainer  */
           thisSearch.songsContainer.innerHTML = generatedHTML;
         }
       } else if (
+        /* if name input has no value and category select has chosen option */
         !thisSearch.input.value &&
-        thisSearch.select.value == thisSearch.select.value
+        thisSearch.categorySelector.value == thisSearch.categorySelector.value
       ) {
+        /* set new empty array named filteredSongs */
         const filteredSongs = [];
+        /* for each song of songs list */
         for (let song of thisSearch.dataSongs) {
-          if (song.categories.includes(thisSearch.select.value)) {
+          /* check if song.categories includes option chosen in category select form */
+          if (song.categories.includes(thisSearch.categorySelector.value)) {
+            /* if it's true push song to filteredSongs array */
             filteredSongs.push(song);
           }
         }
@@ -135,44 +157,53 @@ export class Search {
         } else {
           thisSearch.songsCounter.innerHTML =
             'We have found ' + filteredSongs.length + ' songs...';
-
+          /* based on the song list generate HTML element using template songWrapper */
           const generatedHTML = templates.songWrapper(filteredSongs);
+          /* set generatedHTML as innerHTML of songsContainer  */
           thisSearch.songsContainer.innerHTML = generatedHTML;
         }
       } else if (
+        /* if name input has value and category select has chosen option */
         thisSearch.input.value &&
-        thisSearch.select.value == thisSearch.select.value
+        thisSearch.categorySelector.value == thisSearch.categorySelector.value
       ) {
+        /* set array named songsFilteredByName which filter for each song from songs list */
         const songsFilteredByName = thisSearch.dataSongs.filter((song) => {
           return (
+            /* get song.author name and lowercase it, then merge author name */
             song.author
               .toLowerCase()
               .replaceAll(' ', '')
+              /* check if song.author name includes input value */
               .includes(thisSearch.input.value.toLowerCase()) ||
+            /* and get song.title and lowercase it, then merge song title */
             song.title
               .toLowerCase()
               .replaceAll(' ', '')
+              /* check if song.title includes input value */
               .includes(thisSearch.input.value.toLowerCase())
           );
         });
-
-        const SongsFilteredByCategory = [];
-
+        /* create empty array named songsFilteredByCategory*/
+        const songsFilteredByCategory = [];
+        /* for every song of songsFilteredByName array */
         for (let song of songsFilteredByName) {
-          if (song.categories.includes(thisSearch.select.value)) {
-            SongsFilteredByCategory.push(song);
+          /* if song.categories includes value form category select form */
+          if (song.categories.includes(thisSearch.categorySelector.value)) {
+            /* add song to array named songsFilteredByCategory */
+            songsFilteredByCategory.push(song);
           }
         }
 
-        if (SongsFilteredByCategory.length === 1) {
+        if (songsFilteredByCategory.length === 1) {
           thisSearch.songsCounter.innerHTML =
-            'We have found ' + SongsFilteredByCategory.length + ' song...';
+            'We have found ' + songsFilteredByCategory.length + ' song...';
         } else {
           thisSearch.songsCounter.innerHTML =
-            'We have found ' + SongsFilteredByCategory.length + ' songs...';
+            'We have found ' + songsFilteredByCategory.length + ' songs...';
         }
 
-        const generatedHTML = templates.songWrapper(SongsFilteredByCategory);
+        const generatedHTML = templates.songWrapper(songsFilteredByCategory);
         thisSearch.songsContainer.innerHTML = generatedHTML;
       }
 
@@ -223,25 +254,29 @@ export class Search {
     });
   }
 
-  generateCategoryInputList() {
+  generateCategorySelect() {
     const thisSearch = this;
-
+    /* set empty array called songsCategories */
     const songsCategories = [];
-    thisSearch.categoriesList = document.getElementById(
-      'categories-group-select'
-    );
+    /* for each song of thisSearch.dataSongs */
     for (let song of thisSearch.dataSongs) {
+      /* for each category of song */
       for (let category of song.categories) {
+        /* if the array named songsCategories has no category */
         if (!songsCategories.includes(category)) {
+          /* add category to array named songsCategories */
           songsCategories.push(category);
         }
       }
     }
-
+    /* for each category from songsCategories array  */
     songsCategories.forEach((category) => {
+      /* set variable opt which create element <option></otpion> */
       let opt = document.createElement('option');
+      /* inside option innerText with category  */
       opt.innerText = category;
-      thisSearch.categoriesList.appendChild(opt);
+      /* append category as a child of select form */
+      thisSearch.categorySelector.appendChild(opt);
     });
   }
 
